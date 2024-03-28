6df7b4e9f8513da83f8ecdf8f9cb1315a80652cf
techsameer <sameerkrdbg@gmail.com>
2024-03-28 22:52:43 +0530

    Counter App

---
+++ 03_counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(5)
+
+  // let counter = 5
+
+  const addValue = () => {
+    if (counter >= 20) {
+      window.alert("Maximum value is capped to 20")
+    }
+    else {
+      setCounter(counter + 1)
+    }
+    console.log(counter);
+  }
+
+  const removeValue = () => {
+    if (counter <= 0) {
+      window.alert("Minimum value is capped to 0")
+    }
+    else {
+      setCounter(counter - 1)
+    }
+    console.log(counter);
+  }
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button
+        onClick={addValue}>Add value {counter}</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>footer : {counter}</p>
+    </>
+  )
+}
+
+export default App
