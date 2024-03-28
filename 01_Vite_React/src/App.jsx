05015be552f838b1ad5611d72d3a8d17217981af
techsameer <sameerkrdbg@gmail.com>
2024-03-28 21:44:48 +0530

    React Elements

--- 01_Vite_React/src/App.jsx
+++ 01_Vite_React/src/App.jsx
@@ -2,11 +2,12 @@ import Hello from "./hello"
 
 function App() {
   
+  const userName = "Sameer"
 
   return (
     <>
     <Hello />
-    <h1>Hello React</h1>
+    <h1>Hello React from {userName}</h1>
     <p>Test Paragraph</p>
     </>
   )
