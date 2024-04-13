7b2240a71995f0a945068cfcb5258299a36a25fd
techsameer <sameerkrdbg@gmail.com>
2024-04-13 17:34:39 +0530

    Context

--- 09_Mini_Context/src/context/UserContextProvider.jsx
+++ 09_Mini_Context/src/context/UserContextProvider.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider = ({children}) => {
+const UserContextProvider = ({ children }) => {
     const [user, setUser] = React.useState(null)
-    return(
-        <UserContext.Provider value={{user, setUser}}>
-        {children}
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
         </UserContext.Provider>
     )
 }
